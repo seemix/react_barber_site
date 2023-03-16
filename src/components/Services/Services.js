aabd0529a69ef04aab1e9bb60f432bb65888c46e
seemix <semikhalchuk@gmail.com>
2023-03-16 11:24:25 +0200

    multilanguage added 3 languages

--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -21,19 +21,19 @@ const Services = () => {
                             <h4>{t('haircutStyle')}
                                 {/*<span className={'price'}>180 лей</span>*/}
                             </h4>
-                            <span className={'time'}><AccessTimeIcon style={{ marginRight: '5px' }} fontSize={'18px'}/>45 минут</span>
+                            <span className={'time'}><AccessTimeIcon style={{ marginRight: '5px' }} fontSize={'18px'}/>45 {t('minutes')}</span>
                         </div>
                         <div className={'price_item'}>
                             <h4>{t('beardTrim')}
                                 {/*<span className={'price'}>130 лей</span>*/}
                             </h4>
-                            <span className={'time'}><AccessTimeIcon style={{ marginRight: '5px' }} fontSize={'18px'}/>30 минут</span>
+                            <span className={'time'}><AccessTimeIcon style={{ marginRight: '5px' }} fontSize={'18px'}/>30 {t('minutes')}</span>
                         </div>
                         <div className={'price_item'}>
                             <h4>{t('haircutBeardTrim')}
                                 {/*<span className={'price'}>250 лей</span>*/}
                             </h4>
-                            <span className={'time'}><AccessTimeIcon style={{ marginRight: '5px' }} fontSize={'18px'}/>60 минут</span>
+                            <span className={'time'}><AccessTimeIcon style={{ marginRight: '5px' }} fontSize={'18px'}/>60 {t('minutes')}</span>
                         </div>
                     </div>
                     <div className={'card'}>
@@ -44,20 +44,20 @@ const Services = () => {
                             <h4>{t('shave')}
                                 {/*<span className={'price'}>150 лей</span>*/}
                             </h4>
-                            <span className={'time'}><AccessTimeIcon style={{ marginRight: '5px' }} fontSize={'18px'}/>30 минут</span>
+                            <span className={'time'}><AccessTimeIcon style={{ marginRight: '5px' }} fontSize={'18px'}/>30 {t('minutes')}</span>
                         </div>
                         <div className={'price_item'}>
                             <h4>{t('beardTinting')}
                                 {/*<span className={'price'}>100 лей</span>*/}
                             </h4>
-                            <span className={'time'}><AccessTimeIcon style={{ marginRight: '5px' }} fontSize={'18px'}/>20 минут</span>
+                            <span className={'time'}><AccessTimeIcon style={{ marginRight: '5px' }} fontSize={'18px'}/>20 {t('minutes')}</span>
                         </div>
                         <div className={'price_item'}>
                             {/*<h4>Запись вне рабочих часов <span className={'price'}>100 лей</span></h4>*/}
                             <h4>{t('royalShave')}
                                 {/*<span className={'price'}>100 лей</span>*/}
                             </h4>
-                            <span className={'time'}><AccessTimeIcon style={{ marginRight: '5px' }} fontSize={'18px'}/>30 минут</span>
+                            <span className={'time'}><AccessTimeIcon style={{ marginRight: '5px' }} fontSize={'18px'}/>30 {t('minutes')}</span>
                         </div>
                     </div>
                     <div className={'card'}>
@@ -68,19 +68,19 @@ const Services = () => {
                             <h4>{t('faceWaxing1')}
                                 {/*<span className={'price'}>50 лей</span>*/}
                             </h4>
-                            <span className={'time'}><AccessTimeIcon style={{ marginRight: '5px' }} fontSize={'18px'}/>10 минут</span>
+                            <span className={'time'}><AccessTimeIcon style={{ marginRight: '5px' }} fontSize={'18px'}/>10 {t('minutes')}</span>
                         </div>
                         <div className={'price_item'}>
                             <h4>{t('faceWaxing3')}
                                 {/*<span className={'price'}>150 лей</span>*/}
                             </h4>
-                            <span className={'time'}><AccessTimeIcon style={{ marginRight: '5px' }} fontSize={'18px'}/>25 минут</span>
+                            <span className={'time'}><AccessTimeIcon style={{ marginRight: '5px' }} fontSize={'18px'}/>25 {t('minutes')}</span>
                         </div>
                         <div className={'price_item'}>
                             <h4>{t("hairStyling")}
                                 {/*<span className={'price'}>150 лей</span>*/}
                             </h4>
-                            <span className={'time'}><AccessTimeIcon style={{ marginRight: '5px' }} fontSize={'18px'}/>20 минут</span>
+                            <span className={'time'}><AccessTimeIcon style={{ marginRight: '5px' }} fontSize={'18px'}/>20 {t('minutes')}</span>
                         </div>
                     </div>
                 </div>
