c880d9bbdf8aec5ebedc59550df0130415a9a8a7
seemix <91602641+seemix@users.noreply.github.com>
2024-10-12 18:26:42 +0300

    barbers multi-language about content 1.03

--- src/components/Barbers/SingleBarber/SingleBarber.js
+++ src/components/Barbers/SingleBarber/SingleBarber.js
@@ -6,9 +6,11 @@ import css from './SingleBarber.module.css';
 const SingleBarber = ({ barber }) => {
     return (
         <div className={'card ' + css.barber_card}>
-            <img src={barber.picture} alt={barber.name} loading={'lazy'} className={css.card_picture}/>
-            <h2>{barber.name}</h2>
-            <h3>{barber.description}</h3>
+            <div className={css.card_picture_wrapper}>
+                <img src={barber.picture} alt={barber.name} loading={'lazy'} className={css.card_picture}/>
+                <h2>{barber.name}</h2>
+                <h3>{barber.description}</h3>
+            </div>
             <div> {Parser().parse(barber.about)}</div>
         </div>
 
