010f300fc3f35d49f634ebc834606c87c7b1022c
seemix <91602641+seemix@users.noreply.github.com>
2024-10-12 18:22:50 +0300

    barbers multi-language about content 1.01

--- src/components/Barbers/SingleBarber/SingleBarber.js
+++ src/components/Barbers/SingleBarber/SingleBarber.js
@@ -1,27 +1,14 @@
 import React from 'react';
 import { Parser } from 'html-to-react';
-import { RiStarSFill } from 'react-icons/ri';
 
 import css from './SingleBarber.module.css';
 
 const SingleBarber = ({ barber }) => {
     return (
         <div className={'card ' + css.barber_card}>
-            <div style={{ display: 'flex', flexDirection: 'column', alignContent: 'center' }}>
-                <img src={barber.picture} alt={barber.name} loading={'lazy'} className={css.card_picture}/>
-                {/*<h3>{barber.name}</h3>*/}
-                {/*<p>{barber.description}</p>*/}
-                <div style={{ display: 'flex', gap: '10px', margin: '0 auto' }}>
-                    <RiStarSFill size={'1.6em'} color={'var(--orange)'}/>
-                    <RiStarSFill size={'1.6em'} color={'var(--orange)'}/>
-                    <RiStarSFill size={'1.6em'} color={'var(--orange)'}/>
-                    <RiStarSFill size={'1.6em'} color={'var(--orange)'}/>
-                    <RiStarSFill size={'1.6em'} color={'var(--orange)'}/>
-                </div>
-                <h2>{barber.name}</h2>
-                <h3>{barber.description}</h3>
-
-            </div>
+            <img src={barber.picture} alt={barber.name} loading={'lazy'} className={css.card_picture}/>
+            <h2>{barber.name}</h2>
+            <h3>{barber.description}</h3>
             <div> {Parser().parse(barber.about)}</div>
         </div>
 
