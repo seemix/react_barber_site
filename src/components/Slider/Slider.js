bc48eb1a9bcdc7486d09790059419ed72feea2be
seemix <91602641+seemix@users.noreply.github.com>
2025-12-13 13:52:53 +0200

    slider performance cancellation

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { motion } from 'framer-motion';
 import { Swiper, SwiperSlide } from 'swiper/react';
 // Import Swiper styles
@@ -13,23 +13,23 @@ import { slideAppearAnimation } from './animations';
 import css from './Slider.module.css';
 
 const Slider = ({ slides }) => {
-    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
-    useEffect(() => {
-        const handleResize = () => setWindowWidth(window.innerWidth);
-        window.addEventListener('resize', handleResize);
-        return () => window.removeEventListener('resize', handleResize);
-    }, []);
+    // const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+    //  useEffect(() => {
+    //      const handleResize = () => setWindowWidth(window.innerWidth);
+    //      window.addEventListener('resize', handleResize);
+    //      return () => window.removeEventListener('resize', handleResize);
+    //  }, []);
 
-    const getImageForWidth = ({slide}) => {
-        if (windowWidth <= 480) {
-            return slide.replace(/(\.[a-z]+)$/i, `-480x270$1`);
-        }
-        if (windowWidth <= 768) {
-            return slide.replace(/(\.[a-z]+)$/i, `-768x432$1`);
-        }
-        return slide;
-    };
-       return (
+    // const getImageForWidth = ({slide}) => {
+    //     if (windowWidth <= 480) {
+    //         return slide.replace(/(\.[a-z]+)$/i, `-480x270$1`);
+    //     }
+    //     if (windowWidth <= 768) {
+    //         return slide.replace(/(\.[a-z]+)$/i, `-768x432$1`);
+    //     }
+    //     return slide;
+    // };
+    return (
         <main className={css.slider_container}>
             {slides && <motion.div
                 {...animation}
@@ -38,15 +38,13 @@ const Slider = ({ slides }) => {
                 <Swiper {...swiperSettings}  >
                     {slides &&
                         slides.map((slide, index) => {
-                            const image = getImageForWidth(slide);
+                            //   const image = getImageForWidth(slide);
                             return (
                                 <SwiperSlide key={index}>
                                     <img alt={`slide${index}`} className={css.pic}
-                                         src={image}
+                                         src={slide.slide}
                                          decoding={'async'}
                                     />
-                                    {/*<div className={css.slider_image} style={{ backgroundImage: `url(${image})` }}/>*/}
-                                    {/*<div className={css.slider_overlay}/>*/}
                                 </SwiperSlide>)
                         })
                     }
